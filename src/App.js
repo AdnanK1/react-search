8e73e048158a175ccc8ef416447a5c21a997917d
AdnanK1 <adnangitonga@gmail.com>
2022-08-29 13:03:42 +0300

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
-import {Users} from "./user"
+import { Users } from "./user";
+import { useState } from "react";
+import { Table } from "./Table";
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  const keys = ["first_name", "last_name", "email"];
+
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
+  
   return (
-    <div className="App">
-      <input type="text" placeholder="Search..." className="search" />
-      <ul className="list">
-        {Users.map((user)=>(
-          <li className="listItem">{user}</li>
-        ))}
-      </ul>
+    <div className="app">
+      <input
+        type="text"
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search..."
+        className="search"
+      />
+      <Table data={search(Users)} />
     </div>
   );
 }
